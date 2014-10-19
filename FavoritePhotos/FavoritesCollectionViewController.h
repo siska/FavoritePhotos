e03bfcba253ec4778fb8b375dcdaf3e68439a9d0
Ryan Siska <rsiska1@gmail.com>
2014-10-18 19:42:10 -0500

    New class for Favorites VC set up as type UICollectionVC, CollectionVC added to storyboard, reusable ID set up, class for new VC set to the new file of Favorites VC

---
+++ FavoritePhotos/FavoritesCollectionViewController.h
@@ -0,0 +1,14 @@
+//
+//  FavoritesCollectionViewController.h
+//  FavoritePhotos
+//
+//  Created by S on 10/18/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesCollectionViewController : UICollectionViewController
+@property NSMutableArray *favoritedArray;
+
+@end
